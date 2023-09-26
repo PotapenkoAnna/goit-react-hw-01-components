def2e28b754733cf8afe23b9bb7c89938c1e917d
Potapenko Anna <potapka23@gmail.com>
2023-09-26 11:04:05 +0300

    completed component Profile

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,20 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+import React from 'react'; 
+import { Profile } from './Profile/Profile';
+import user from '../data/user.json' 
+//import css from './App.module.css';
+ 
+export const App = () => {   
+  return ( 
+    
+      <div>  
+        <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location} 
+        avatar={user.avatar}
+        stats={user.stats}
+        />      
+      </div> 
+     
   );
 };
